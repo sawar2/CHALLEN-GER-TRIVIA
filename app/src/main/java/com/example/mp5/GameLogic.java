da11d3774f10d4d6a1f286ffbdfddcbe615aa924
sawar2 <sawar2@illinois.edu>
2019-04-30 10:45:02 -0500

    Added Phrase Class

--- app/src/main/java/com/example/mp5/GameLogic.java
+++ app/src/main/java/com/example/mp5/GameLogic.java
@@ -4,34 +4,36 @@ import java.util.Random;
 
 public class GameLogic {
     private int player1Points = 0;
-    private String[] arrayOfPhrases = String[10];
+    private int player2Points = 0;
 
-
-    public void phraseInitialization() {
-        arrayOfPhrases[0] = "red";
-        arrayOfPhrases[1] = "orange";
-        arrayOfPhrases[2] = "yellow";
-        arrayOfPhrases[3] = "green";
-        arrayOfPhrases[4] = "blue";
-        arrayOfPhrases[5] = "indigo";
-        arrayOfPhrases[6] = "violet";
-        arrayOfPhrases[7] = "purple";
-        arrayOfPhrases[8] = "pink";
-        arrayOfPhrases[9] = "gray";
-    }
-
+    //Random Number Generator
     public int getRandomNumber() {
         double randomNumber = 10 * Math.random();
         int numberAsInt = (int) randomNumber;
         return numberAsInt;
     }
 
-    public int setPlayer1Points() {
-        int player1Points += 1;
+    //Set the number of points for player 1
+    public void setPlayer1Points(int points) {
+        player1Points = points;
     }
 
-    public void getPlayer1Points() {
-        return player1Points;
+    //Get the number of points for player 1
+    public int getPlayer1Points() {
+        return player2Points;
     }
+
+    //Set the number of points for player 2
+    public void setPlayer2Points(int points) {
+        player2Points = points;
+    }
+
+    //Get the number of points for player 2
+    public int getPlayer2Points() {
+        return player2Points;
+    }
+
+
+
 
 }
