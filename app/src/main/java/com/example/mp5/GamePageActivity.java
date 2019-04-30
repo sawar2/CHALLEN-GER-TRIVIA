30593770ed49fb80b1beb57dd2d8017402208472
sawar2 <sawar2@illinois.edu>
2019-04-30 12:24:07 -0500

    UI Changed and start button working

---
+++ app/src/main/java/com/example/mp5/GamePageActivity.java
@@ -0,0 +1,14 @@
+package com.example.mp5;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GamePageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gamepage);
+
+    }
+}
--- app/src/main/java/com/example/mp5/MainActivity.java
+++ app/src/main/java/com/example/mp5/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.mp5;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.startpage);
+
+        configureStartButton();
+    }
+
+    public void configureStartButton() {
+        Button startButton = (Button) findViewById(R.id.startButton);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, GamePageActivity.class));
+
+            }
+        });
     }
 }
