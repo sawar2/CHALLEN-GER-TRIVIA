226c30200c0a2016198c571722b1e61e009231f7
sawar2 <sawar2@illinois.edu>
2019-04-30 22:18:50 -0500

    Everything fundamentally finished, need to code incorrect button choices, player point label, sound, and a few other things,

--- app/src/main/java/com/example/mp5/MainActivity.java
+++ app/src/main/java/com/example/mp5/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends WinPageActivity {
+public class MainActivity extends AppCompatActivity {
 
     private boolean isPlayer1 = true;
     public boolean getPlayer1() {
